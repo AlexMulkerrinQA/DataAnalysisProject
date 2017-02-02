85143db1d3e72d08839ff3a5a7b436081b85d386
AlexMulkerrinQA <Alexander.Mulkerrin@qa.com>
2017-02-02 15:14:25 +0000

    More intelligent article getter script

--- getArticle.py
+++ getArticle.py
@@ -1,39 +1,52 @@
-import os, time
+import os, time, re
 
-print ("finding article count...")
+print ("finding article count...\n")
 startTime = time.clock()
 numArticles = 0
-maxLines = 400
+maxLines = 4000
 outFile = open('articleText.txt', 'w')
 with open("../wikiDataset/enwiki-20090810-pages-articles.xml") as inFile:
 	isArticle = False
 	isRedirect = False
-	isTitle = False
+	articleTitle = ""
 	isTextContent = False
 	for i in range(maxLines):
 		line = inFile.readline()
 		if line.find("</page>") > 0:
 			isArticle = False
 			isTextContent = False
+			isRedirect = False
+			articleTitle = "none"
 
 		if isArticle:
 			if line.find("<text") > 0: # note text element has attributes attached
 				isTextContent = True
-			if isTextContent:
-				print ".",
-				#outFile.write(line)
-				
+				if not isRedirect:
+					print articleTitle,
+					numArticles += 1
+
+				#	print "(R)",
+				#print ""
+			if isTextContent and not isRedirect:
+				text = re.sub("<text[^<]+?>|</text>", '', line)
+				print text
+				print outFile.write(text)
+
 			#check for title
-			if line.find("<title>")
-			print re.sub('<[^<]+?>', '', line)
+			if line.find("<title>") > 0:
+				articleTitle = re.sub('<[^<]+?>|\n|\t|^ +', '', line)
+
+			#check if page is redirect
+			if line.find("<redirect />") > 0:
+				isRedirect = True
 
 		if line.find("</text>") > 0:
 			isTextContent = False
 
 		if line.find("<page>") > 0:
 			isArticle = True
-			numArticles += 1
+#			numArticles += 1
 
 endTime = time.clock()
-print("Count complete in: " + str(endTime-startTime) )
+print("\nCount complete in: " + str(endTime-startTime) )
 print("Articles found: " + str(numArticles) )
