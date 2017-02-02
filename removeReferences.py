621f0ee863e2ffdd60bd8dae180f09e10aa8476c
AlexMulkerrinQA <Alexander.Mulkerrin@qa.com>
2017-02-02 11:59:31 +0000

    Regex power, go!

--- removeReferences.py
+++ removeReferences.py
@@ -1,53 +1,29 @@
 import os, re, HTMLParser
 
 inFile = open('cleanedText.txt','r')
-#outFile = open('textOnly.txt', 'w')
-#sentences = re.split(r'\n', inFile.read())
+outFile = open('textOnly.txt', 'w')
 
-wholeText = inFile.read()
-
-isTag = False
-#isTextContent = False	
+wholeText = inFile.read()	
 # regexs are confusing... remove single tags and text between ref tag pairs.
 # captures opening wiki markup '[[' (with or without | pipe), closing ']]', single html tags <x />, reference elements <ref x>xxx</ref>
-regexPattern = """\[\[[^|^\]]+\||
-	\[\[|
-	\]\]|
-	<[^>]+/>|
-	<ref.*?>[\s\S]*?</ref>|
-	={1,6}"""
-
-#try do this compiled thing, still todo:
-#\[\[(File|Category):[\s\S]+\]\]|
-#{{[\s\S\n]+?}}|
-
-#(<s>|<!--)[\s\S]+(</s>|-->)|
 CompiledRegex = re.compile(r"""
+		\[\[(Image|File):[^\[]+\||
+		\[\[Category:[^\]]+\]\]|
 		\[\[[^|^\]]+\||
 		\[\[|
 		\]\]|
 		\'{2,5}|
 		\{\{[^\}]+\}\}|
+		\[[^\]]+\]|
 		<[^>]+/>|
 		<ref.*?>[\s\S]*?</ref>|
+		<!--.*?-->|
 		={1,6}""", re.VERBOSE)
 	
-#for i in range(5):
-	#sentence = sentences[i]
-	# out = ""
-	# for c in sentence:
-		# if c == '<':
-			# isTag = True
-		# elif c == '>':
-			# isTag = False
-			
-		# elif not isTag:
-			# out += c
-out = CompiledRegex.sub("_", wholeText)
+out = CompiledRegex.sub("", wholeText)
+#print out
+outFile.write(out)
 
-print out
-
-
-#outFile.close()
+outFile.close()
 inFile.close()
 
