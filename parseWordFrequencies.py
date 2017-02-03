1a2b2c11bfa3415c35212b71750a14e7430fed69
AlexMulkerrinQA <Alexander.Mulkerrin@qa.com>
2017-02-03 16:25:56 +0000

    moved files around and done word freq on 100 articles

--- parseWordFrequencies.py
+++ parseWordFrequencies.py
@@ -1,34 +1,40 @@
 import os, re, operator
 
-wordList = {}
-allWordsTotal = 0
+def findWordFreq(inFileName):
+	inFile = open('articleTexts/' + inFileName, 'r')
+	sentences = re.split(r'\n', inFile.read().lower())
+	wordList = {}
+	allWordsTotal = 0
 
-print ("Parsing file for word frequencies...")
+	for sentence in sentences:
+		words = re.findall(r"[\w']+", sentence)
+		for word in words:
+			allWordsTotal += 1
+			if not (word in wordList):
+				wordList[word] = 1
+			else:
+				wordList[word] += 1
 
-inFile = open('textOnly.txt','r')
-sentences = re.split(r'\n', inFile.read().lower())
+	sortedWordList = sorted(wordList.items(), key=operator.itemgetter(1), reverse=True)
+	return sortedWordList
 
-for sentence in sentences:
-	words = re.findall(r"[\w']+", sentence)
-	for word in words:
-		allWordsTotal += 1
-		if not (word in wordList):
-			wordList[word] = 1
+def displayWordList(sortedWordList):
+	isFirst = True
+	output = ""
+	for i in range(20):
+		word = sortedWordList[i]
+		if not isFirst:
+			output += ', '
 		else:
-			wordList[word] += 1
+			isFirst = False
+		output += '"'+str(word[0])+'":'+str(word[1])
+	output += '}\n'
+	print output
 
-print ("Parsed " + str(allWordsTotal) + " words. With " + str(len(wordList)) + " different words encountered")
-
-sortedWordList = sorted(wordList.items(), key=operator.itemgetter(1), reverse=True)
-
-print '"Anarchism"{'
-isFirst = True
-output = ""
-for word in sortedWordList:
-	if not isFirst:
-		output += ', '
-	else:
-		isFirst = False
-	output += '"'+str(word[0])+'":'+str(word[1])
-output += '\n}'
-print output
+for inFileName in os.listdir('articleTexts'):
+	articleTitle = re.sub('.txt', '', inFileName)
+	print articleTitle
+	
+	wordList = findWordFreq(inFileName)
+	displayWordList(wordList)
+	
